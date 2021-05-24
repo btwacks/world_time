bc47fd32f17077446ca73fefbd2f918e0bd14824
Brandon Wackerman <brandon.wackerman@gmail.com>
2021-05-23 19:02:07 -0500

    Created ternary operators so that images displayed will differ between day and night.
    Also added day and night asset images.

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:world_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 
 class Loading extends StatefulWidget {
@@ -18,6 +19,7 @@ class _LoadingState extends State<Loading> {
       'location': instance.location,
       'flag': instance.flag,
       'time': instance.time,
+      'isDaytime': instance.isDaytime,
     });
 
   }
@@ -31,9 +33,12 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(50.0),
-        child: Text('loading'),
+      backgroundColor: Colors.blue[900],
+      body: Center(
+        child: SpinKitFadingCube(
+          color: Colors.white,
+          size: 80.0,
+        ),
       ),
     );
   }
--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -7,7 +7,8 @@ class WorldTime {
   String location; // location name for the UI
   String time;     // time in that location
   String flag;     // url to an asset flag icon
-  String url;       // location url for api endpoint
+  String url;      // location url for api endpoint
+  bool isDaytime;  // True or False if daytime or not
 
   WorldTime({ this.location, this.flag, this.url });
 
@@ -30,6 +31,7 @@ class WorldTime {
       now = now.add(Duration(hours: int.parse(offset)));
 
       // set the time property
+      isDaytime = now.hour > 6 && now.hour < 20 ? true : false;
       time = DateFormat.jm().format(now);
     }
     catch (e) {
